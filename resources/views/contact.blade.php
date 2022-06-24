a168014c2cbaba97feb53dd87494c48d416ed22c
Précieux Assako <assakoprecieux19@gmail.com>
2022-06-24 17:40:14 +0100

    Formulaire de contact opérationnel

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -56,31 +56,7 @@
                     </div><!-- End Google Maps -->
 
                     <div class="col-lg-6">
-                        <form action="forms/contact.php" method="post" role="form" class="php-email-form">
-                            <div class="row gy-4">
-                                <div class="col-lg-6 form-group">
-                                    <input type="text" name="name" class="form-control" id="name"
-                                        placeholder="Votre Nom" required>
-                                </div>
-                                <div class="col-lg-6 form-group">
-                                    <input type="email" class="form-control" name="email" id="email"
-                                        placeholder="Votre Email" required>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <input type="text" class="form-control" name="subject" id="subject" placeholder="Sujet"
-                                    required>
-                            </div>
-                            <div class="form-group">
-                                <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
-                            </div>
-                            <div class="my-3">
-                                <div class="loading">Loading</div>
-                                <div class="error-message"></div>
-                                <div class="sent-message">Votre message a été envoyé. Merci !</div>
-                            </div>
-                            <div class="text-center"><button type="submit">Envoyez le Message</button></div>
-                        </form>
+                        @livewire('contact')
                     </div><!-- End Contact Form -->
 
                 </div>
